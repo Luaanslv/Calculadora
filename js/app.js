b14a0cea9c1d4961096882750448a09df54f8380
Luaanslv <luaan.silva117@gmail.com>
2021-11-25 15:08:42 -0300

    historico da calculadora

--- js/app.js
+++ js/app.js
@@ -1,26 +1,37 @@
+var numero = document.getElementById("resultado").innerHTML
 function insert(num){
-   const numero = document.getElementById('resultado').innerHTML;
-   document.getElementById('resultado').innerHTML = numero + num;
-
+    numero = document.getElementById("resultado").innerHTML;
+   document.getElementById("resultado").innerHTML = numero + num ;
+        
+}
+function addhistor(res, resultado) {
+    let histor = document.getElementById("resultado").innerHTML;
+    if(histor){
+        document.getElementById("res").innerHTML= `${numero} = ${histor}`;
+    }else{
+        document.getElementById('res').innerHTML = "Nada...";
+    }
+    
 }
 
-function clean(){
+    function clean(){
     document.getElementById('resultado').innerHTML = "";
-}
+    
+    }
 
-function back(){
-    const resultado = document.getElementById('resultado').innerHTML;
+    function back(){
+    var resultado = document.getElementById('resultado').innerHTML;
     document.getElementById('resultado').innerHTML = resultado.substring(0,resultado.length -1);
-}
-function calcular(){
-    const resultado = document.getElementById('resultado').innerHTML;
+    }
+    function calcular(){
+    var resultado = document.getElementById('resultado').innerHTML;
     
     if(resultado){
+        numero = document.getElementById('resultado').innerHTML;
         document.getElementById('resultado').innerHTML = eval(resultado);
     }else{
         document.getElementById('resultado').innerHTML = "Nada...";
     }
-
-}
-
     
+}
+   
